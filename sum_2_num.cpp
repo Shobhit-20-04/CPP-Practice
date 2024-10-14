7a7dd94ed9828610c4a3b46e015cfc3c06077ed5
Shobhit <shobhitagrawal2004@gmail.com>
2024-10-14 22:57:05 +0530

    Basic addition of 2 numbers

---
+++ sum_2_num.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, num3;
+    cout << "Enter first number=";
+    cin >> num1;
+    cout << "Enter second number=";
+    cin >> num2;
+    num3 = num1 + num2;
+    cout << "Sum of " << num1 << " and " << num2 << " is " << num3;
+    return 0;
+}
