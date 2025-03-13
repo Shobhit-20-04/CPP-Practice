f020f20570c0440bc9cb69c2bddc44e01ba4d8d9
Shobhit <shobhitagrawal2004@gmail.com>
2025-03-13 23:56:18 +0530

    Pattern Questions

--- pat1.cpp
+++ pat1.cpp
@@ -3,9 +3,18 @@ using namespace std;
 int main()
 {
     int i, j;
+    // Forward Loop
     for (i = 1; i <= 5; i++)
     {
         for (j = 1; j <= 6 - i; j++)
+            cout << j;
+        cout << "\n";
+    }
+    cout << "\n"; // New Line
+    // Backward Loop
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
             cout << j;
         cout << "\n";
     }
