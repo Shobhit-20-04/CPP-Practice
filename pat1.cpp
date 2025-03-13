bbd475532e9ce0c14356c88fcb0ff24332321ac8
Shobhit <shobhitagrawal2004@gmail.com>
2025-03-13 23:53:43 +0530

    Pattern Questions

---
+++ pat1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 6 - i; j++)
+            cout << j;
+        cout << "\n";
+    }
+    return 0;
+}
