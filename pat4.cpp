f1a7ef402f7d5b49cb37e8c1ea9e718ab97e50e7
Shobhit <shobhitagrawal2004@gmail.com>
2025-03-15 23:37:26 +0530

    Pattern Questions

---
+++ pat4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j;
+    // Forward Loop
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 6 - i; j >= 1; j--)
+            cout << j;
+        cout << "\n";
+    }
+    cout << "\n"; // New Line
+    //Backward Loop
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = i; j >= 1; j--)
+            cout << j;
+        cout << "\n";
+    }
+    return 0;
+}
