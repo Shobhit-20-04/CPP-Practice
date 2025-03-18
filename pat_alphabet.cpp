f5aca73aaf25e07d5e3c709f2e6de94afb23d286
Shobhit <shobhitagrawal2004@gmail.com>
2025-03-19 02:49:27 +0530

    Created patterns of alphabet except P, Q, R, V, W, Y

--- pat_alphabet.cpp
+++ pat_alphabet.cpp
@@ -75,7 +75,6 @@ void patE()
         cout << "\n";
     }
 }
-
 void patF()
 {
     int i, j;
@@ -84,6 +83,306 @@ void patF()
         for (j = 0; j <= 7; j++)
         {
             if (i == 0 || i == 3 || j == 0)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patG()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (i == 0 || i == 6 || j == 0 || (i >= 3 && j == 7) || (i == 3 && j >= 4))
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patH()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (j == 7 || i == 3 || j == 0)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patI()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 8; j++)
+        {
+            if (i == 0 || i == 6 || j == 4)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patJ()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 8; j++)
+        {
+            if (i == 0 || j == 4 || (i == 6 && j <= 4) || (j == 0 && i >= 5))
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patK()
+{
+    int i, j;
+    for (i = 0; i <= 8; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (j == 0 || i + j == 5 || i - j == 3)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patL()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (i == 6 || j == 0)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patM()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 6; j++)
+        {
+            if (j == 0 || j == 6 || (i == j && i <= 3) || (i <= 3 && i + j == 6))
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patN()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (j == 0 || j == 7 || i == j)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patO()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (i == 0 || i == 6 || j == 0 || j == 7)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patP()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (i == 0 || i == 6 || j == 0 || j == 7)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patQ()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (i == 0 || i == 6 || j == 0 || j == 7)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patR()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (i == 0 || i == 6 || j == 0 || j == 7)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patS()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (i == 0 || i == 6 || i == 3 || (j == 0 && i <= 3) || (j == 7 && i >= 3))
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patT()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 8; j++)
+        {
+            if (i == 0 || j == 4)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patU()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (i == 6 || j == 0 || j == 7)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patV()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (i == 0 || i == 6 || j == 0 || j == 7)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patW()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (i == 0 || i == 6 || j == 0 || j == 7)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patX()
+{
+    int i, j;
+    for (i = 0; i <= 7; i++)
+    {
+        for (j = 0; j <= 6; j++)
+        {
+            if (i == j || i + j == 6)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patY()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (i == 0 || i == 6 || j == 0 || j == 7)
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << "\n";
+    }
+}
+void patZ()
+{
+    int i, j;
+    for (i = 0; i <= 6; i++)
+    {
+        for (j = 0; j <= 7; j++)
+        {
+            if (i == 0 || i == 6 || i + j == 7)
                 cout << "*";
             else
                 cout << " ";
@@ -111,6 +410,46 @@ int main()
             patE();
         else if (ch == 'f' || ch == 'F')
             patF();
+        else if (ch == 'g' || ch == 'G')
+            patG();
+        else if (ch == 'h' || ch == 'H')
+            patH();
+        else if (ch == 'i' || ch == 'I')
+            patI();
+        else if (ch == 'j' || ch == 'J')
+            patJ();
+        else if (ch == 'k' || ch == 'K')
+            patK();
+        else if (ch == 'l' || ch == 'L')
+            patL();
+        else if (ch == 'm' || ch == 'M')
+            patM();
+        else if (ch == 'n' || ch == 'N')
+            patN();
+        else if (ch == 'o' || ch == 'O')
+            patO();
+        else if (ch == 'p' || ch == 'P')
+            patP();
+        else if (ch == 'q' || ch == 'Q')
+            patQ();
+        else if (ch == 'r' || ch == 'R')
+            patR();
+        else if (ch == 's' || ch == 'S')
+            patS();
+        else if (ch == 't' || ch == 'T')
+            patT();
+        else if (ch == 'u' || ch == 'U')
+            patU();
+        else if (ch == 'v' || ch == 'V')
+            patV();
+        else if (ch == 'w' || ch == 'W')
+            patW();
+        else if (ch == 'x' || ch == 'X')
+            patX();
+        else if (ch == 'y' || ch == 'Y')
+            patY();
+        else if (ch == 'z' || ch == 'Z')
+            patZ();
 
     } while (ch != 'Q');
     return 0;
